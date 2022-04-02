a3f65fd82c56badff9839032b09065c08f430b61
kierzek <kierzek@student.agh.edu.pl>
2022-04-02 13:32:41 +0200

    basic components

---
+++ hakathon20/src/dev/header.js
@@ -0,0 +1,12 @@
+import './header.css'
+
+
+const Header = () => {
+    return(
+        <div className='Header'>
+
+        </div>
+    )
+}
+
+export default Header
---
+++ hakathon20/src/dev/login.js
@@ -0,0 +1,10 @@
+import './login.css';
+
+
+const Login = () => {
+    return(
+        <div></div>
+    );
+}
+
+export default Login
